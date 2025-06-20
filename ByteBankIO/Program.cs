ebb3e8f990ff312883dd3429e75e353a1980ab40
VickyPrado <vicky.prado05@gmail.com>
2025-06-20 15:25:54 -0300

    ler-dados-em-partes-com-buffer

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,19 +1,34 @@
 ﻿using ByteBankIO;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
         var caminhoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+        
         var fluxoAquivo = new FileStream(caminhoArquivo, FileMode.Open);
 
         var buffer = new byte[1024]; // 1KB
 
-        fluxoAquivo.Read(buffer, 0, 1024);
+
+        while(numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoAquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }        
 
         //public override int Read(byte[] array, int offset, int count)
-
-
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
     }
 }
