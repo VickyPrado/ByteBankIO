e23e498236f9f0fd0458ed76d6dd29e3afda4b28
VickyPrado <vicky.prado05@gmail.com>
2025-07-09 17:39:45 -0300

    organizando-code-separando-FileStream-em-novo-arquivo-e-add-partial-para-class-Program-permitindo-que-seja-dividida-em-multiplos-arquivos

---
+++ ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,42 @@
+﻿using ByteBankIO;
+using System.Data;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente()
+    {
+        var caminhoArquivo = "contas.txt";
+
+        using (var fluxoAquivo = new FileStream(caminhoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+            var buffer = new byte[1024]; // 1KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoAquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+
+            //public override int Read(byte[] array, int offset, int count)
+            fluxoAquivo.Close();
+            Console.ReadLine();
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bitesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bitesLidos);
+        Console.Write(texto);
+
+        /*
+        foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+        */
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -2,41 +2,10 @@
 using System.Data;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
-    {
-        var caminhoArquivo = "contas.txt";
-
-        using (var fluxoAquivo = new FileStream(caminhoArquivo, FileMode.Open))
-        {
-            var numeroDeBytesLidos = -1;
-            var buffer = new byte[1024]; // 1KB
-
-            while (numeroDeBytesLidos != 0)
-            {
-                numeroDeBytesLidos = fluxoAquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer, numeroDeBytesLidos);
-            }
-
-            //public override int Read(byte[] array, int offset, int count)
-            fluxoAquivo.Close();
-            Console.ReadLine();
-        }        
-    }
-
-    static void EscreverBuffer(byte[] buffer, int bitesLidos)
-    {
-        var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer, 0, bitesLidos);
-        Console.Write(texto);
-
-        /*
-        foreach(var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
-        }
-        */
-    }
+    {        
+        Console.ReadLine();
+    }    
 }
