619b28739cf1b4669fd0c230af67aefe72df15dc
VickyPrado <vicky.prado05@gmail.com>
2025-07-15 22:41:24 -0300

    add-nova-classe-CriarArquivo-em-csv-e-nova-classe-passando-confs-StreamReader

---
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4785.40, Gustavo Santos";
+
+            var encoding = Encoding.UTF8;
+
+            var bytes = Encoding.UTF8.GetBytes(contaComoString);
+
+            fluxoArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
